350ca3467891a48bd2174c2bd3f9d11367469764
Serhii Sydorenko <sidorchik91@gmail.com>
2023-09-09 13:33:56 +0300

    TEST | new config

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,31 @@ import App from './App.jsx'
 import './index.css'
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from "wagmi/providers/public";
 import { sepolia } from "wagmi/chains";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 const { VITE_ALCHEMY_API_KEY } = import.meta.env;
 
-const connector = new MetaMaskConnector({
+const metaMaskConnector = new MetaMaskConnector({
 	chains: [sepolia],
 });
 
+const injectedConnector = new InjectedConnector({chains: [sepolia],})
+
+
 const { publicClient, webSocketPublicClient } = configureChains(
 	[sepolia],
-	[alchemyProvider({ apiKey: VITE_ALCHEMY_API_KEY })]
+	[alchemyProvider({ apiKey: VITE_ALCHEMY_API_KEY })],
+	publicProvider(),
 );
 
 const config = createConfig({
 	autoConnect: true,
 	publicClient,
 	webSocketPublicClient,
-	connectors: [connector],
+	connectors: [metaMaskConnector, injectedConnector],
 });
 
 
