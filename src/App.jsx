c74c2b3b38bb1914bec8af4885b1b923b8b4e160
Serhii Sydorenko <sidorchik91@gmail.com>
2023-09-14 21:04:06 +0300

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const SharedLayout = lazy(() => import("@/components/SharedLayout"));
 function App() {
 	return (
 		<Suspense fallback={<Fallback />}>
-			<BrowserRouter basename="/">
+			<BrowserRouter basename="/stake">
 				<Routes>
 					<Route path="/" element={<Navigate to="/stake" />} />
 					<Route path="/" element={<SharedLayout />}>
