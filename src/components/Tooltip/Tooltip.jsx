cbe9428622618ec4d416285c1d2fea5d10c2fcf5
Serhii Sydorenko <sidorchik91@gmail.com>
2023-09-24 15:55:58 +0300

    ADD | mobile and tablet versions of tooltip

---
+++ src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import {
+	Container,
+	SubContainer,
+	Graber,
+	Title,
+	Description,
+} from "./Tooltip.styled";
+// import Draggable from "react-draggable";
+
+function DraggableTooltip({ title, description, setIsShown, isShown }) {
+	const [isTooltipShown, setIsToolTipShown] = useState(true);
+
+	const handleClose = (e) => {
+		e.preventDefault();
+		setIsToolTipShown(false);
+		setTimeout(() => {
+			setIsShown(false);
+		}, 500);
+	};
+
+	return (
+		<>
+			{isShown && (
+				<Container
+					onClick={(e) => {
+						handleClose(e);
+					}}
+				>
+					<SubContainer
+						draggable={true}
+						onDragStart={(e) => {
+							handleClose(e);
+						}}
+						$isshown={isTooltipShown}
+					>
+						<Graber></Graber>
+						<Title>{title}</Title>
+						<Description>{description}</Description>
+					</SubContainer>
+				</Container>
+			)}
+		</>
+	);
+}
+
+export default DraggableTooltip;
---
+++ src/components/Tooltip/Tooltip.styled.js
@@ -0,0 +1,85 @@
+import styled, { keyframes, css } from "styled-components";
+import { media } from "@/utils/mediaRules";
+import { StyledButton, Paragraph } from "@/components/SharedElements.styled";
+
+export const Container = styled.div`
+	background: rgba(0, 0, 0, 0.6);
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 998;
+    display: flex;
+`;
+
+const slideInAnimation = keyframes`
+    0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+
+const slideOutAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(100%);
+  }
+`;
+
+export const SubContainer = styled.div`
+	width: 100%;
+	height: 275px;
+	background-color: var(--color-white);
+	z-index: 999;
+	margin-top: auto;
+	border-radius: 8px 8px 0px 0px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding-left: 16px;
+	padding-right: 16px;
+    animation-iteration-count: 1;
+
+${({ $isshown }) => {
+    if ($isshown) {
+        return css`
+					animation: ${slideInAnimation} 0.5s ease-in;
+				`;
+    } else {
+        return css`
+					animation: ${slideOutAnimation} 0.5s ease-out;
+				`;
+    }
+}}
+`;
+
+export const SubContainerShown = styled.div`
+
+`
+
+export const Graber = styled.div`
+	width: 36px;
+	height: 5px;
+	margin-top: 8px;
+	margin-bottom: 11px;
+	border-radius: 2.5px;
+	background-color: var(--color-grabber);
+`;
+
+export const Title = styled.h3`
+	font: var(--font-tooltip-title-mobile-tablet);
+    color: var(--color-black);
+    z-index: 999;
+    margin-bottom: 16px;
+`;
+
+export const Description = styled(Paragraph)`
+	color: var(--color-black);
+`;
+
