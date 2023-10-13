c367a3fc7127289b7e15e2cd77019640c338d0b2
Serhii Sydorenko <sidorchik91@gmail.com>
2023-10-13 15:22:25 +0300

    Made some changes with env

--- src/utils/hooks/useTransaction.js
+++ src/utils/hooks/useTransaction.js
@@ -5,7 +5,7 @@ import { formatEther } from "viem";
 const { VITE_STRU_STAKING_CONTRACT } = import.meta.env;
 
 // This hook recieves all the data from the useContractWriteOperations hook,
-//  and makes all the transaction async requests. 
+//  and makes all the transaction async requests.
 // It returns async functions to be used in Form component.
 
 function useTransaction() {
@@ -19,24 +19,30 @@ function useTransaction() {
 	} = useContractWriteOperations();
 	const [isApprovalTransactionLoading, setIsApprovalTransactionLoading] =
 		useState(false);
+	const [apporvalTransactionHash, setApporvalTransactionHash] = useState("");
 
 	const stake = async (value) => {
 		const allowance = Number(formatEther(allowanceAmount));
 		// Before doing the 'stake' method, we check the allowance amount and,
 		// if it is less than the amount of the current transaction, we do the 'approve' method and then the 'stake' method.
 		if (Number(formatEther(value)) > allowance) {
-			 // using this state to display whether the process of approval or staking taking place
+			// using this state to display whether the process of approval or staking taking place
 			// receiving the hash and passing it to waitForTransaction method to create a Promise and wait for the transaction to get done
 			try {
 				setIsApprovalTransactionLoading(true);
 				const { hash: approvalHash } = await approvalWrite({
 					args: [VITE_STRU_STAKING_CONTRACT, value],
 				});
+				setApporvalTransactionHash(approvalHash);
 				const { status: approvalStatus } = await waitForTransaction({
 					hash: approvalHash,
 				});
 				// when the transaction is done we recieve a result of Promise and proceed with the next, "stake" method
-				approvalStatus && setIsApprovalTransactionLoading(false);
+				if (approvalStatus) {
+					setApporvalTransactionHash("");
+					setIsApprovalTransactionLoading(false);
+				}
+				
 				if (approvalStatus === "success") {
 					const { hash: stakeHash } = await stakeWrite({
 						args: [value],
@@ -46,10 +52,10 @@ function useTransaction() {
 						hash: stakeHash,
 					});
 					return response;
-				} 
+				}
 			} catch (error) {
 				setIsApprovalTransactionLoading(false);
-				throw new Error(error)
+				throw new Error(error);
 			}
 			// If the allowance >= current operation's amount we immediately proceed with "stake" method
 		} else {
